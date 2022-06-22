ed1e751c0d4ec277c6f5fcf41d2d5718ffa9acdd
Nikita Bochkovskiy <nikita.bochkovsky@yandex.ru>
2022-06-22 18:51:21 +0600

    Добавьте файлы проекта.

---
+++ Q12/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Q12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сколько элементов будет в массиве?");
+            int count = Convert.ToInt32(Console.ReadLine());
+            var array = new int[count];
+            for (int i= 0;i<count;i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Все элементы записаны");
+        }
+    }
+}
