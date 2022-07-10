a0035c844ba1b0561c293827c9e8f8fa4c8e8a2d
Nikita Bochkovskiy <nikita.bochkovsky@yandex.ru>
2022-07-10 18:39:03 +0600

    Задание 13.2.5

--- Q12/Program.cs
+++ Q12/Program.cs
@@ -1,21 +1,36 @@
 ﻿using System;
 using System.IO;
+using System.Collections;
+
 namespace Q12
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // читаем весь файл с рабочего стола в строку текста
-            string text = File.ReadAllText("C:\\Users\\Eugene\\Desktop\\Text.txt");
+            var months = new[]
+            {
+              "Jan", "Feb", "Mar", "Apr", "May" , "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+            };
 
-            // Сохраняем символы-разделители в массив
-            char[] delimiters = new char[] { ' ', '\r', '\n' };
+            var numbers = new[]
+            {
+              1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12
+            };
+            var arraylist = new ArrayList();
 
-            // разбиваем нашу строку текста, используя ранее перечисленные символы-разделители
-            var words = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
-            // выводим количество
-            Console.WriteLine(words.Length);
+            foreach (var number in numbers)
+            {
+                // добавляем в ArrayList строку месяца (начинаем с нулевого по индексу)
+                arraylist.Add(months[number - 1]);
+
+                // добавляем его порядковый номер
+                arraylist.Add(number);
+            }
+
+            foreach (var value in arraylist)
+                Console.WriteLine(value);
         }
+
     }
 }
