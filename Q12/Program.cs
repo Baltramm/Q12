7b3a2439c12c656f9cf9368a89acb5c3d3cea7e8
Nikita Bochkovskiy <nikita.bochkovsky@yandex.ru>
2022-07-11 17:39:13 +0600

    Задание 13.3.4

--- Q12/Program.cs
+++ Q12/Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Collections;
 using System.Text;
+using System.Collections.Generic;
 
 namespace Q12
 {
@@ -9,41 +10,53 @@ namespace Q12
     {
         static void Main(string[] args)
         {
-            var arrayList = new ArrayList()
-           {
-               1,
-               "Андрей ",
-               "Сергей ",
-               300,
-           };
-            int sum = 0;
-            StringBuilder text = new StringBuilder();
-            foreach (var element in arrayList)
-            {
-                //   если целое число - увеличиваем счётчик
-                if (element is int)
-                {
-                    sum += (int)element;
-                }
+            //  создаём пустой список с типом данных Contact
+            var phoneBook = new List<Contact>();
 
-                // если строка - добавляем текст из неё
-                if (element is string s)
-                {
-                    text.Append(element);
-                }
-            }
+            // добавляем контакты
+            phoneBook.Add(new Contact("Игорь", 79990000000, "igor@example.com"));
+            phoneBook.Add(new Contact("Андрей", 79990000001, "andrew@example.com"));
 
-            // результат
-            var result = new ArrayList() { sum, text.ToString() };
-
-            // вывод
-            foreach (var elem in result)
-            {
-                Console.WriteLine(elem);
-            }
+            // проверяем, что добавилось в список
+            foreach (var contact in phoneBook)
+                Console.WriteLine(contact.Name + ": " + contact.PhoneNumber);
         }
 
-       
+        public class Contact // модель класса
+        {
+            public Contact(string name, long phoneNumber, String email) // метод-конструктор
+            {
+                Name = name;
+                PhoneNumber = phoneNumber;
+                Email = email;
+            }
+
+            public String Name { get; }
+            public long PhoneNumber { get; }
+            public String Email { get; }
+        }
+        private static void AddUnique(Contact newContact, List<Contact> phoneBook)
+        {
+            bool alreadyExists = false;
+            foreach (var contact in phoneBook)
+            {
+                if (contact.Name == newContact.Name)
+                {
+                   alreadyExists = true;
+                    break;
+                }
+
+            }
+            if (!alreadyExists)
+            {
+                phoneBook.Add(newContact);
+            }
+            phoneBook.Sort((x, y) => String.Compare(x.Name, y.Name, StringComparison.Ordinal));
+
+            foreach (var contact in phoneBook)
+                Console.WriteLine(contact.Name + ": " + contact.PhoneNumber);
+
+        }
 
     }
 }
