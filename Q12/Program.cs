9b43a2799aa261a72acee01de60125324c0bd844
Nikita Bochkovskiy <nikita.bochkovsky@yandex.ru>
2022-07-11 18:06:50 +0600

    Задание 13.3.10

--- Q12/Program.cs
+++ Q12/Program.cs
@@ -10,53 +10,22 @@ namespace Q12
     {
         static void Main(string[] args)
         {
-            //  создаём пустой список с типом данных Contact
-            var phoneBook = new List<Contact>();
 
-            // добавляем контакты
-            phoneBook.Add(new Contact("Игорь", 79990000000, "igor@example.com"));
-            phoneBook.Add(new Contact("Андрей", 79990000001, "andrew@example.com"));
 
-            // проверяем, что добавилось в список
-            foreach (var contact in phoneBook)
-                Console.WriteLine(contact.Name + ": " + contact.PhoneNumber);
-        }
+            var sentence =
+    "Подсчитайте, сколько уникальных символов в этом предложении, используя HashSet<T>, учитывая знаки препинания, но не учитывая пробелы в начале и в конце предложения.";
+            // сохраняем в массив char
+            var characters = sentence.ToCharArray();
 
-        public class Contact // модель класса
-        {
-            public Contact(string name, long phoneNumber, String email) // метод-конструктор
-            {
-                Name = name;
-                PhoneNumber = phoneNumber;
-                Email = email;
-            }
+            var symbols = new HashSet<char>();
 
-            public String Name { get; }
-            public long PhoneNumber { get; }
-            public String Email { get; }
-        }
-        private static void AddUnique(Contact newContact, List<Contact> phoneBook)
-        {
-            bool alreadyExists = false;
-            foreach (var contact in phoneBook)
-            {
-                if (contact.Name == newContact.Name)
-                {
-                   alreadyExists = true;
-                    break;
-                }
+            // добавляем во множество. Сохраняются только неповторяющиеся символы
+            foreach (var symbol in characters)
+                symbols.Add(symbol);
 
-            }
-            if (!alreadyExists)
-            {
-                phoneBook.Add(newContact);
-            }
-            phoneBook.Sort((x, y) => String.Compare(x.Name, y.Name, StringComparison.Ordinal));
-
-            foreach (var contact in phoneBook)
-                Console.WriteLine(contact.Name + ": " + contact.PhoneNumber);
+            // Выводим результат
+            Console.WriteLine(symbols.Count);
 
         }
-
     }
 }
