3de865144897a21cd5710da7a049a29358d510a0
Nikita Bochkovskiy <nikita.bochkovsky@yandex.ru>
2022-07-11 18:10:40 +0600

    Задание 13.3.11

--- Q12/Program.cs
+++ Q12/Program.cs
@@ -19,10 +19,24 @@ namespace Q12
 
             var symbols = new HashSet<char>();
 
+            var signs = new[] { ',', ' ', '.' };
+
+            // сохраняем числовые символы в массив Char
+            var numbers = new[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+
             // добавляем во множество. Сохраняются только неповторяющиеся символы
             foreach (var symbol in characters)
+            {
+                
                 symbols.Add(symbol);
+            }
+            bool containsNumbers = symbols.Overlaps(numbers);
+            Console.WriteLine($"Коллекция содержит цифры: {containsNumbers}");
 
+            // Отбрасываем знаки препинания и заново считаем
+            symbols.ExceptWith(signs);
+            Console.WriteLine($"Символов без знаков препинания:: {symbols.Count}");
             // Выводим результат
             Console.WriteLine(symbols.Count);
 
